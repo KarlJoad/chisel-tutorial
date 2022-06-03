8442c51c042292a8be4d97ae646f51f82e0ae792
Karl Hallsby <karl@hallsby.com>
2022-06-03 10:10:25 -0500

    Implement RealGCD module, by looking at solution
    I included a number of comments because I need to explain to myself
    how these higher-level modules work, such as DeqIO, which is based on
    the DecoupledIO class.

--- src/main/scala/problems/RealGCD.scala
+++ src/main/scala/problems/RealGCD.scala
@@ -19,8 +19,35 @@ class RealGCD extends Module {
     val out = Output(Valid(UInt(16.W)))
   })
 
-  // Implement below ----------
+  val x = Reg(UInt())
+  val y = Reg(UInt())
+  // We need this control register to handle the difference between reading
+  // values in for GCD-ing and actually performing the GCD.
+  // Remember, we are writing an HDL here, so all "when" blocks will technically
+  // "run" simultaneously. We need a way to ctonrol which one runs.
+  val should_gcd = RegInit(false.B)
 
-  // Implement above ----------
+  // We need to signal when this GCD module is ready to consume data from the queue
+  io.in.ready := !should_gcd
 
+  // Read in the values
+  // The DeqIO class comes with 'valid' and 'ready' flags for synchronization
+  when (io.in.valid && !should_gcd) {
+    x := io.in.bits.a
+    y := io.in.bits.b
+    should_gcd := true.B
+  }
+
+  when (should_gcd) {
+    when (x > y) { x := y; y := x }
+      .otherwise   { y := y - x }
+  }
+
+  // The Valid class adds a valid bit to some bits that are data.
+  io.out.bits := x
+  // Output is only valid once y has reached 0, while should_gcd is true
+  io.out.valid := (y === 0.U) && should_gcd
+  when (io.out.valid) {
+    should_gcd := false.B // Reset control bit once we have finished the process
+  }
 }
