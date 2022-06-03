d9b170e7a723fe4d366f35fa161f55241a63854a
Karl Hallsby <karl@hallsby.com>
2022-06-03 11:07:21 -0500

    Add test for Max2 module, testing all possible maximums

--- src/test/scala/problems/Max2Tests.scala
+++ src/test/scala/problems/Max2Tests.scala
@@ -8,15 +8,12 @@ import chisel3.iotesters.PeekPokeTester
 // Implement test with PeekPokeTester
 //
 class Max2Tests(c: Max2) extends PeekPokeTester(c) {
-  for (i <- 0 until 10) {
-
-    // Implement below ----------
-
-    poke(c.io.in0, 0)
-    poke(c.io.in1, 0)
-    step(1)
-    expect(c.io.out, 1)
-
-    // Implement above ----------
+  for (i <- 0 until 1 << 8) {
+    for (j <- 0 until 1 << 8) {
+      poke(c.io.in0, i)
+      poke(c.io.in1, j)
+      step(1)
+      expect(c.io.out, (i).max(j))
+    }
   }
 }
